854de793385525880c810d154120ad7db8789837
B-loeb <bradleyloeb@gmail.com>
2022-02-05 19:40:24 -0500

    JS-Callback-Practice Homework
    Thankyou for taking the extra time to help us out guys! I really appreciate it!

--- move.js
+++ move.js
@@ -1,12 +1,67 @@
+ //the function curlies stay open for this first one and encase all code on this file//
 function move(element) {
     element.style.position = 'fixed'
-
+                 
+//this function body is closed//
     function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
+//this functions curlies are also left open and encase everything but the last Four lines of code//
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+//This function invokes the character to move based on if statements nested within the block //
+        function moveCharacter(){ 
+            if(direction === 'west'){
+                x = x - 1
+            }
+            if(direction === 'north'){
+                y = y + 1
+            }
+            if(direction === 'east'){
+                x = x + 1
+            }
+            if(direction === 'south'){
+                y = y - 1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            callback(direction)
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
+    }
 
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
